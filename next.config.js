e62ce7821539cd0ae92efd85234cd8a69ac1c90e
Crys <mariano.christopher123@gmail.com>
2022-11-28 22:18:04 +0800

    updated image loader

--- next.config.js
+++ next.config.js
@@ -3,8 +3,8 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
-    loader: "akamai",
-    path: "",
+    loader: "cloudinary",
+    path: "https://github.com/crys-cross/landingpage-a/tree/main/assets",
   },
 };
 
